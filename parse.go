fe3117d7cb9b549986ffd91b0551cd7092bb18f4
Jochen Voss <voss@seehuhn.de>
2015-01-09 14:47:09 +0000

    add functions for parsing of HTTP headers

---
+++ parse.go
@@ -0,0 +1,149 @@
+package httputil
+
+import (
+	"errors"
+	"unicode/utf8"
+)
+
+var (
+	ErrMissingComma               = errors.New("missing comma")
+	ErrUnexpectedControlCharacter = errors.New("unexpected control character")
+	ErrUnexpectedQuotedString     = errors.New("unexpected quoted string")
+	ErrUnterminatedEscape         = errors.New("unterminated escape")
+	ErrUnterminatedString         = errors.New("unterminated string")
+)
+
+func isTSpecial(c rune) bool {
+	for _, d := range "()<>@,;:\\\"/[]?={} \t" {
+		if c == d {
+			return true
+		}
+	}
+	return false
+}
+
+func isCtl(c rune) bool {
+	return c < 32 || c == 127
+}
+
+func tokenizeHeader(header string) ([]string, error) {
+	res := []string{}
+
+	start := 0
+	quoted := false
+	escaped := false
+
+	runes := []rune(header)
+	for pos, c := range runes {
+		if quoted {
+			if escaped {
+				escaped = false
+			} else if c == '\\' {
+				escaped = true
+			} else if c == '"' {
+				quoted = false
+				res = append(res, string(runes[start:pos+1]))
+				start = pos + 1
+			}
+		} else if isTSpecial(c) {
+			if pos > start {
+				res = append(res, string(runes[start:pos]))
+			}
+
+			start = pos + 1
+			if c == '"' {
+				quoted = true
+				start = pos
+			} else if c != ' ' && c != '\t' {
+				res = append(res, string(runes[pos:pos+1]))
+			}
+		} else if isCtl(c) {
+			return nil, ErrUnexpectedControlCharacter
+		}
+	}
+
+	if escaped {
+		return nil, ErrUnterminatedEscape
+	} else if quoted {
+		return nil, ErrUnterminatedString
+	} else if start < len(runes) {
+		res = append(res, string(runes[start:]))
+	}
+
+	return res, nil
+}
+
+// NormalizeHeader converts a HTTP header value into a standard form
+// by removing all optional white-space.
+func NormalizeHeader(value string) string {
+	res := ""
+	tokens, err := tokenizeHeader(value)
+	if err != nil {
+		return value
+	}
+	for _, token := range tokens {
+		t, _ := utf8.DecodeRuneInString(token)
+		if t == utf8.RuneError {
+			return value
+		}
+		if res == "" || isTSpecial(t) {
+			res += token
+		} else {
+			t, _ = utf8.DecodeLastRuneInString(res)
+			if t == utf8.RuneError {
+				return value
+			}
+			if isTSpecial(t) {
+				res += token
+			} else {
+				res += " " + token
+			}
+		}
+	}
+	return res
+}
+
+type HeaderPart struct {
+	Key, Value string
+}
+
+// ParseHeader parses HTTP an header value.  This function only works
+// for headers which are defined to be comma-separated lists of tokens
+// and key-value pairs, e.g. for the Vary and Cache-Control headers.
+func ParseHeader(value string) ([]HeaderPart, error) {
+	tokens, err := tokenizeHeader(value)
+	if err != nil {
+		return nil, err
+	}
+
+	res := []HeaderPart{}
+
+	requireComma := false
+
+	i := 0
+	n := len(tokens)
+	for i < n {
+		if tokens[i] == "," {
+			i++
+			requireComma = false
+			continue
+		}
+		if requireComma {
+			return nil, ErrMissingComma
+		}
+		if tokens[i][0] == '"' {
+			return nil, ErrUnexpectedQuotedString
+		}
+
+		part := HeaderPart{Key: tokens[i]}
+		i++
+		if i+1 < n && tokens[i] == "=" {
+			part.Value = tokens[i+1]
+			i += 2
+		}
+		res = append(res, part)
+		requireComma = true
+	}
+
+	return res, nil
+}
---
+++ parse_test.go
@@ -0,0 +1,130 @@
+// headers_test.go - unit tests for headers.go
+// Copyright (C) 2014  Jochen Voss <voss@seehuhn.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package httputil
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNormalizeHeader(t *testing.T) {
+	type testCase struct {
+		in, out string
+	}
+	table := []testCase{
+		{"", ""},
+		{" ", ""},
+		{"a", "a"},
+		{"*", "*"},
+		{"a  b", "a b"},
+		{"a,b", "a,b"},
+		{"a, b", "a,b"},
+		{"a  ,  b", "a,b"},
+		{"a=b", "a=b"},
+		{"a= b", "a=b"},
+		{"a  =  b", "a=b"},
+		{`a = "b  c"`, `a="b  c"`},
+	}
+	for _, test := range table {
+		res := NormalizeHeader(test.in)
+		if res != test.out {
+			t.Errorf("normalizing %q: expected %q, got %q",
+				test.in, test.out, res)
+		}
+	}
+}
+
+func TestTokenizeHeader(t *testing.T) {
+	type testCase struct {
+		in  string
+		out []string
+		err error
+	}
+
+	table := []testCase{
+		{"", []string{}, nil},
+		{" ", []string{}, nil},
+		{"a", []string{"a"}, nil},
+		{"aaaa", []string{"aaaa"}, nil},
+		{"a bb ccc", []string{"a", "bb", "ccc"}, nil},
+		{"\"\\", nil, ErrUnterminatedEscape},
+		{"\"", nil, ErrUnterminatedString},
+		{"\r", nil, ErrUnexpectedControlCharacter},
+		{"\t", []string{}, nil},
+		{" \t a  \t\t  ", []string{"a"}, nil},
+		{`"a  \" b" c`, []string{`"a  \" b"`, "c"}, nil},
+		{"<>", []string{"<", ">"}, nil},
+	}
+
+	for _, test := range table {
+		res, err := tokenizeHeader(test.in)
+		if err != test.err {
+			t.Errorf("tokenizing %q returned error '%v' instead of '%v'",
+				test.in, err, test.err)
+		} else if !reflect.DeepEqual(res, test.out) {
+			t.Errorf("tokenizing %q returned result '%#v' instead of '%#v'",
+				test.in, res, test.out)
+		}
+
+		in2 := NormalizeHeader(test.in)
+		if len(in2) > len(test.in) {
+			t.Errorf("%q normalized to longer %q", test.in, in2)
+		}
+		res, err = tokenizeHeader(in2)
+		if err != test.err {
+			t.Errorf("tokenizing %q returned error '%v' instead of '%v'",
+				in2, err, test.err)
+		} else if !reflect.DeepEqual(res, test.out) {
+			t.Errorf("tokenizing %q returned result '%#v' instead of '%#v'",
+				in2, res, test.out)
+		}
+	}
+}
+
+func TestParseHeader(t *testing.T) {
+	type parseTest struct {
+		in  string
+		out []HeaderPart
+		err error
+	}
+
+	table := []parseTest{
+		{"", []HeaderPart{}, nil},
+		{"\"", nil, ErrUnterminatedString},
+		{"\"hello\"", nil, ErrUnexpectedQuotedString},
+		{"a", []HeaderPart{{"a", ""}}, nil},
+		{"a=1", []HeaderPart{{"a", "1"}}, nil},
+		{"a=\"1\"", []HeaderPart{{"a", "\"1\""}}, nil},
+		{" ,,,, , ,, , , ", []HeaderPart{}, nil},
+		{"a,b", []HeaderPart{{"a", ""}, {"b", ""}}, nil},
+		{"a=1,b,c=2", []HeaderPart{{"a", "1"}, {"b", ""}, {"c", "2"}}, nil},
+		{"a,b c,d", nil, ErrMissingComma},
+		{"a,b=,d", nil, ErrMissingComma},
+		{"a,b=c d,e", nil, ErrMissingComma},
+	}
+
+	for _, test := range table {
+		res, err := ParseHeader(test.in)
+		if err != test.err {
+			t.Errorf("parsing %q returned error '%v' instead of '%v'",
+				test.in, err, test.err)
+		} else if !reflect.DeepEqual(res, test.out) {
+			t.Errorf("parsing %q returned result '%#v' instead of '%#v'",
+				test.in, res, test.out)
+		}
+	}
+}
